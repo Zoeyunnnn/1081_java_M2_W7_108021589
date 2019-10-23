0531d7e3ab8b300d15b4edc60a2bb8a689352055
Zoeyunnnn <hicandy0406@gmail.com>
2019-10-23 22:49:11 +0800

    First Commit

--- src/M2Q25.java
+++ src/M2Q25.java
@@ -5,6 +5,25 @@ public class M2Q25 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-
+        int sum = 0,ct;
+        boolean flag = true;
+        for (int i=2;i<=n;i++){
+            ct = i;
+            for (;ct!=1;){
+                if (ct%2==0){
+                    ct /= 2;
+                }else if (ct%3==0){
+                    ct /= 3;
+                }else if (ct%5==0){
+                    ct /= 5;
+                }else {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)sum +=i;
+            flag = true;
+        }
+        System.out.println(sum);
     }
 }
