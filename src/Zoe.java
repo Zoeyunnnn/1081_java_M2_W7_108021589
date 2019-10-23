eb4936e4da4bbdf205c6a7c4907dc35ff3fc6e72
Zoeyunnnn <hicandy0406@gmail.com>
2019-10-23 15:43:52 +0800

    First commit

---
+++ src/Zoe.java
@@ -0,0 +1,66 @@
+import java.util.*;
+public class Zoe {
+    public static void main(String[] args) {
+
+//1加到100的總和
+//    int i,sum = 0;
+//	for (i=1;i<=100;i++){
+//	    sum = sum + i;
+//    }
+// 	    System.out.println(sum);
+
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        int i,j = 0;
+//        for (i=a;i<=b;i++){
+//           j = j + i;
+//            System.out.println(j);
+//        }
+
+//M2Q8正整數被3整除的總和
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int i,sum=0;
+//        for (i=3;i<=n;i+=3){    /*也可使用if限制，但較冗長*/
+//            sum += i;
+//        }
+//        System.out.println(sum);
+
+//M2Q1最大值最小值
+//    Scanner sc = new Scanner(System.in);
+//    float v1 = sc.nextFloat();
+//    float max = v1;
+//    float min = v1;
+//
+//    for (int i=1;i<9;i++){
+//        v1 = sc.nextFloat();
+//        if (max<v1){
+//            max = v1;
+//        }else if (min>v1){
+//            min = v1;
+//        }
+//    }
+//        System.out.printf("%.2f\n",max);
+//        System.out.printf("%.2f\n",min);
+
+//M2Q6質數判定
+//    Scanner sc = new Scanner(System.in);
+//    int n = sc.nextInt();
+//    int m = n/2;
+//    boolean flog = true;
+//
+//    for (int i=2;i<m;i++){
+//        if (n%i == 0){
+//            flog = false;
+//            break;
+//        }
+//    }
+//    if (flog){
+//        System.out.println("Yes");
+//    }else {
+//        System.out.println("No");
+//    }
+    }
+}
