3e2957b19c3c7e10e2ff13f5c0e89f4a847a617e
Zoeyunnnn <hicandy0406@gmail.com>
2019-10-23 16:38:02 +0800

    First Commit

---
+++ src/M2Q14.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class M2Q14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i=0;i<=n;i++){
+            sum += i*(i+1);
+        }
+        System.out.println(sum);
+
+    }
+}
