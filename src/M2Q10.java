8764dea1c0ec11b368825b584b46e2df1e8905ff
Zoeyunnnn <hicandy0406@gmail.com>
2019-10-23 21:38:36 +0800

    First Commit

---
+++ src/M2Q10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class M2Q10{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0,i=1,j=1;
+        for (i=1;i<=n;i++){
+            for (j=1;j<n;j++){
+              sum = i*j;
+                System.out.print(sum+"\t");
+            }
+            System.out.println(i*(j));
+        }
+    }
+}
